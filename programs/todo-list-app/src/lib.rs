85dd0e7fb2bc0cb107ea3901b57d06a246f0ce17
twicedee <okumudesy@gmail.com>
2023-12-05 11:23:28 +0300

    initial commit

---
+++ programs/todo-list-app/src/lib.rs
@@ -0,0 +1,102 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Di9oVZnSe1jTW8tzxyaQyP4aiSgDu2p9FFbpr5Q2vez4");
+
+
+
+#[program]
+pub mod todo_list_app {
+
+    use super::*;
+
+    pub fn adding_task(ctx: Context<AddingTask>, text: String) -> Result<()> {
+        let task = &mut ctx.accounts.task;
+        let author = &ctx.accounts.author;
+        let clock = Clock:: get().unwrap();
+        if text.chars().count() > 400{
+            return Err(ErrorCode::TextTooLong.into());
+        }
+        task.author = *author.key;
+        task.is_done = false;
+        task.created_at = clock.unix_timestamp;
+        task.updated_at = clock.unix_timestamp;
+        task.text = text;
+        Ok(())
+    }
+    pub fn updating_task(ctx: Context<UpdatingTask>, is_done: bool) -> Result<()>{
+        let task = &mut ctx.accounts.task;
+        let author = &ctx.accounts.author;
+        let clock = Clock::get().unwrap();
+
+        task.author = *author.key;
+        task.is_done = is_done;
+        task.updated_at = clock.unix_timestamp;
+        Ok(())
+    }
+    pub fn deleting_task(ctx: Context<DeletingTask>) -> Result<()>{
+        let task = &mut ctx.accounts.task;
+        let author = &ctx.accounts.author;
+        let clock = Clock::get().unwrap();
+
+        task.author = *author.key;
+        task.is_done = true;
+        task.updated_at = clock.unix_timestamp;
+        Ok(())
+    }
+    
+}
+
+#[derive(Accounts)]
+pub struct AddingTask<'info> {
+    #[account(init, payer = author, space = Task::LEN)]
+    pub task: Account<'info, Task>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct UpdatingTask<'info> {
+    #[account(mut, has_one = author)]
+    pub task: Account<'info, Task>,
+    pub author : Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct DeletingTask<'info> {
+    #[account(mut, has_one = author)]
+    pub task: Account<'info, Task>,
+    pub author : Signer<'info>,
+}
+
+#[account]
+pub struct Task{
+    pub author: Pubkey,  //account that owns the tast
+    pub is_done: bool,  // weather the tast is done
+    pub text: String,  //the text of the task
+    pub created_at: i64,  //timestamp when created
+    pub updated_at: i64,  //timestamp wen last updated
+}
+
+const DISCRIMINATOR: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const BOOL_LENGTH: usize = 1;
+const TEXT_LENGTH: usize = 4 + 400 * 4; // 400 chars
+const TIMESTAMP_LENGTH: usize = 8;
+
+impl Task {
+    const LEN: usize = DISCRIMINATOR + // discriminator
+        PUBLIC_KEY_LENGTH + // author
+        BOOL_LENGTH + // is_done
+        TEXT_LENGTH +  // text
+        TIMESTAMP_LENGTH + // created_at
+        TIMESTAMP_LENGTH; // updated_at
+}
+
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The test is too long!")]
+    TextTooLong,
+}
+
